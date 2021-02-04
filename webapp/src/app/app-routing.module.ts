3a44f6cd7a2f2a935368a15b61a2e8f0d04e608a
_danre_ <iamdanre@gmail.com>
2021-02-04 04:35:21 +0200

    auth
    login and JWT authentication

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+
+const routes: Routes = [
+{ path: 'home', component: HomeComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'user', component: BoardUserComponent },
+{ path: 'admin', component: BoardAdminComponent },
+{ path: '', redirectTo: 'home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
